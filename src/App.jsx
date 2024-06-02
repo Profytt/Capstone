97be987ee1e2e46896a38b975aeb29d3778bc15c
Profytt <dezcoleman2@gmail.com>
2024-06-02 07:02:43 -0500

    navbar commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
 import Products from './components/Products.jsx';
 import ProductDetail from './components/ProductDetails.jsx';
 import { ApiProvider } from './components/Api';
@@ -8,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <ApiProvider>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Products />} />
           <Route path="/product/:productId" element={<ProductDetail />} /> 
